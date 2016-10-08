610b695b0b9ace98532d431f6858696c856d0c24
nddquang <1312458@student.hcmus.edu.vn>
2016-10-08 12:44:27 +0700

    add username field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # call the configured params
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # protect the database, while allowing these fields to be updated
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :remember_me)}
+  end
 end
