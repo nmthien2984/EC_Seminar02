55155dd431f2af13b5f67cf87cfc4575b6350ea5
nddquang <1312458@student.hcmus.edu.vn>
2016-10-08 16:59:56 +0700

    create post

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,9 @@ class PagesController < ApplicationController
     else
       redirect_to root_path, :notice=> "User not found."
     end
+
+    @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+    @newPost = Post.new
   end
 
   def explore
